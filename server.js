cb09d67bfbd7a85b3910b30c831685b3096af0d3
JamesFs4 <james.f.smith@wsu.edu>
2022-11-28 09:55:42 -0800

    Server Adjustments
    Added ability to pull mongo database

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-// app.use(cors());
-
-// app.use('/login', (req, res) => {
-//     res.send({
-//         token: 'test123'
-//     });
-// });
-
-// app.listen(8080, () => console.log('API is running http://localhost:8080/login'));
+const { MongoClient } = require('mongodb')
+const connectionString = 'mongodb+srv://jfsmith5:Test@cluster0.pt7mltf.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(connectionString);
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.listen(3000, () => {
@@ -25,18 +18,22 @@ app.post('/quotes', (req, res) => {
     console.log(req.body)
 })
 
-MongoClient.conenct('mongodb-connection-string', (err, client)
-{
-    mongodb+srv://jfsmith5:Test@cluster0.pt7mltf.mongodb.net/?retryWrites=true&w=majority
-    if (err) return console.error(err)
-    console.log('Connected to Database')
 
-})
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://jfsmith5:<password>@cluster0.pt7mltf.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+async function run() {
+    try 
+    {
+        console.log('Connected to Database')
+        // const database = client.db('testDatabase');
+        // const col = database.collection('testData');
+        const books = client.db('testDatabase')
+        // const query = {testField: "testVar"};
+        // const data = await col.findOne(query);
+
+        console.log(books);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
